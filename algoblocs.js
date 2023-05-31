83d56ab6d45c63c7ed5f51b7ce213c7fc634941f
fcroux31 <nobrainnogaincoaching@gmail.com>
2023-05-31 16:44:43 +0200

    push exo js

---
+++ algoblocs.js
@@ -0,0 +1,246 @@
+/*
+// SERIE A
+// A1
+// INITIALISAION
+setPos(300, 300);
+changeColor(color.red);
+setLineWidth(10);
+
+// REALISATION
+forward(200);
+faceRight();
+forward(100);
+*/
+
+// SERIE A
+// A2
+// INITIALISATION
+// setPos(300, 300);
+// changeColor(color.yellow);
+// setLineWidth(10)
+// faceLeft();
+// forward(100);
+// faceDown();
+// forward(100);
+// faceRight();
+// forward(200);
+// faceUp();
+// forward(200);
+
+// SERIE A
+// A3
+// setPos(300, 300);
+// changeColor(color.red);
+// setLineWidth(10);
+// faceLeft();
+// forward(100);
+// faceRight();
+// forward(200);
+// faceLeft();
+// forward(100);
+// faceUp();
+// forward(100);
+// faceDown();
+// forward(200);
+
+// SERIE A
+// A4
+// setPos(250, 400);
+// changeColor(color.green);
+// setLineWidth(10);
+// faceRight();
+// forward(100);
+// arcLeft(50,90);
+// arcLeft(50,90);
+// faceLeft();
+// forward(50);
+// arcRight(50,90);
+// arcRight(50,90);
+// faceRight();
+// forward(100);
+
+// SERIE A
+// A5
+// setPos(300, 100);
+// changeColor(color.fuchsia);
+// setLineWidth(10);
+// faceRight();
+// arcRight(200,360);
+// arcRight(100,180);
+// arcLeft(100,180);
+
+// SERIE B
+// B1
+// setPos(100, 100);
+// changeColor(color.green);
+// setLineWidth(10);
+// faceRight();
+// // BOUCLE
+// for( let i = 0 ; i < 4 ; i++) {
+//     forward(100);
+//     faceDown();
+//     forward(100);
+//     faceRight();
+// }
+
+// SERIE B
+// B2
+// setPos(100, 300);
+// changeColor(color.red);
+// setLineWidth(10);
+
+// for( let i = 0 ; i < 4 ; i++) {
+//     faceUp();
+//     forward(50);
+//     faceRight();
+//     forward(50);
+//     faceDown();
+//     forward(50);
+//     faceRight();
+//     forward(50);
+// }
+
+// SERIE B
+// B3
+// setPos(400, 200);
+// changeColor(color.blue);
+// setLineWidth(10);
+
+// for( let i = 0 ; i < 4 ; i++) {
+//     forward(200);
+//     right(90);
+// }
+
+// SERIE B
+// B4
+// setPos(200, 400);
+// changeColor(color.aqua);
+// setLineWidth(10);
+// faceRight();
+
+// for( let i = 0 ; i < 3 ; i++) {
+//     forward(200);
+//     left(120);
+// }
+
+// SERIE B
+// B5
+// setPos(300, 400);
+// changeColor(color.brown);
+// setLineWidth(10);
+// faceUp();
+// forward(200);
+// changeColor(color.red);
+
+// for( let i = 0 ; i < 3 ; i++) {
+//     forward(100);
+//     right(120);
+// }
+
+// SERIE B
+// B6
+// setPos(50, 300);
+// changeColor(color.red);
+// setLineWidth(10);
+// faceRight();
+
+// for( let i = 0 ; i < 2 ; i++) {
+//     forward(100);
+//     faceUp();
+//     forward(100);
+//     faceRight();
+// }
+
+// changeColor(color.green);
+
+// for( let i = 0 ; i < 3 ; i++) {
+//     forward(100);
+//     faceDown();
+//     forward(100);
+//     faceRight();
+// }
+
+// SERIE C
+// C1
+// setPos(100, 300);
+// changeColor(color.aqua);
+// setLineWidth(10);
+// faceRight();
+// for( let i = 0 ; i < 4 ; i++) {
+//     for( let j = 0 ; j < 3 ; j++) {
+//         forward(100);
+//         right(120);
+//     }
+//     forward(100);
+// }
+
+// SERIE C
+// C2
+// setPos(200, 500);
+// changeColor(color.fuchsia);
+// setLineWidth(10);
+// faceUp();
+// for( let i = 0 ; i < 5 ; i++) {
+//     for( let j = 0 ; j < 4 ; j++) {
+//         forward(50);
+//         right(90);
+//     }
+// up();
+// forward(100);
+// down();
+// }
+
+// SERIE C
+// C3
+// setPos(50, 100);
+// shiftColor(1/3);
+// setLineWidth(10);
+// faceRight();
+// for( let i = 0 ; i < 3 ; i++){
+//     for( let j = 0 ; j < 8 ; j++) {
+//         forward(100);
+//         left(135);
+//     }
+// up();
+// forward(200);
+// shiftColor(0.1);
+// down();
+// }
+
+// SERIE C
+// C4
+// setPos(50, 300);
+// setLineWidth(10);
+// faceRight();
+// changeColor(color.red);
+// for( let i = 0 ; i < 5 ; i++) {
+//     for( let j = 0 ; j < 3 ; j++) {
+//         forward(50);
+//         left(120);
+//     }
+//     changeColor(color.black)
+//     for( let k = 0 ; k < 4 ; k++) {
+//         forward(50);
+//         right(90);
+//     }
+//     up();
+//     forward(100);
+//     changeColor(color.red);
+//     down();
+// }
+
+// SERIE C
+// C5
+// setPos(300, 300);
+// setLineWidth(10);
+// faceRight();
+// shiftColor(1/5);
+// for( let i = 0 ; i < 8 ; i++) {
+//     for( let j = 0 ; j < 8 ; j++) {
+//         forward(200);
+//         left(135);
+//     }
+//     shiftColor(0.25);
+//     right(90);
+// }
+
